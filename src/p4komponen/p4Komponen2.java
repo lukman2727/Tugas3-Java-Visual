36123088229b2261954a286ed9425f2349245227
Lukman Kurniawan <campione47@gmail.com>
2017-07-22 07:39:52 +0700

    Tugas3 JavaIII(Visual)

---
+++ src/p4komponen/p4Komponen2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p4komponen;
+
+import p4komponen.Framenya.MainFrm2;
+
+/**
+ *
+ * @author USER
+ */
+public class p4Komponen2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrm2 i = new MainFrm2();
+        i.setVisible(true);
+    }
+    
+}
